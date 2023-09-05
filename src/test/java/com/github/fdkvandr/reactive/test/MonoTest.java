30e8733ee8c5e9a272cee62e19e0a0b99e24def1
Andrey Fedyakov <ya.andreyfedyakov@yandex.ru>
2023-09-06 00:12:32 +0300

    test: added test to Mono

--- src/test/java/com/github/fdkvandr/reactive/test/MonoTest.java
+++ src/test/java/com/github/fdkvandr/reactive/test/MonoTest.java
@@ -2,12 +2,21 @@ package com.github.fdkvandr.reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @Slf4j
 public class MonoTest {
 
     @Test
-    public void test() {
-        log.info("Everything working as intended");
+    public void monoSubscriber() {
+        String name = "Andrey Fedyakov";
+        Mono<String> mono = Mono.just(name)
+                .log();
+        mono.subscribe();
+        log.info("------------------------------");
+        StepVerifier.create(mono)
+                .expectNext(name)
+                .verifyComplete();
     }
 }
