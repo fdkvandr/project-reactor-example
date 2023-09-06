a2a62eace7867cbf71b3e22d7174ef19bc3d8249
Andrey Fedyakov <ya.andreyfedyakov@yandex.ru>
2023-09-06 18:26:16 +0300

    test: added test to Flux: from, fromIterable, error, custom couple subscriber

--- src/test/java/com/github/fdkvandr/reactive/test/FluxTest.java
+++ src/test/java/com/github/fdkvandr/reactive/test/FluxTest.java
@@ -2,8 +2,12 @@ package com.github.fdkvandr.reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+
+import java.util.List;
 
 @Slf4j
 public class FluxTest {
@@ -28,5 +32,71 @@ public class FluxTest {
         StepVerifier.create(flux)
                 .expectNext(1, 2, 3, 4, 5)
                 .verifyComplete();
+    }
+
+    @Test
+    public void fluxSubscriberFromList() {
+        Flux<Integer> flux = Flux.fromIterable(List.of(1, 2, 3, 4 ,5)).log();
+
+        flux.subscribe(it -> log.info("Number: {}", it));
+        log.info("------------------------------");
+
+        StepVerifier.create(flux)
+                .expectNext(1, 2, 3, 4, 5)
+                .verifyComplete();
+    }
+
+    @Test
+    public void fluxSubscriberNumbersError() {
+        Flux<Integer> flux = Flux.range(1, 5)
+                .map(it -> {
+                    if (it == 4) throw new IndexOutOfBoundsException("Index error");
+                    return it;
+                })
+                .log();
+
+        flux.subscribe(
+                it -> log.info("Number: {}", it),
+                Throwable::printStackTrace,
+                () -> log.info("DONE!"),
+                subscription -> subscription.request(3));
+    }
+
+    @Test
+    public void fluxSubscriberNumbersUglyBackpressure() {
+
+        Flux<Integer> flux = Flux.range(1, 10)
+                .log();
+
+        flux.subscribe(new Subscriber<>() {
+
+            private int count = 0;
+            private Subscription subscription;
+            private final int requestCount = 2;
+
+
+            @Override
+            public void onSubscribe(Subscription subscription) {
+                this.subscription = subscription;
+                subscription.request(requestCount);
+            }
+
+            @Override
+            public void onNext(Integer item) {
+                count++;
+                if (count >= requestCount) {
+                    count = 0;
+                    subscription.request(requestCount);
+                }
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+            }
+
+            @Override
+            public void onComplete() {
+            }
+        });
     }
 }
